797277036704cfe6fa5b34634104d9fb125a9d86
sitanshmehta <s65mehta@uwaterloo.ca>
2023-11-07 20:37:33 -0500

    Setting up foundation for home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './Home.css'
+import profile from './profile.png';
+
 
 const Home = () => {
     return(
-        <div className="home-page">
-            <header>
-                <h1>Home Page</h1>
-            </header>
+        <div className='home-page'>
+            <div className='intro'>
+                <span> Hey I'm <h1 className='name'> Sitansh Mehta</h1> </span>
+            </div>
+            <div>
+                <img src={profile} alt='Whoops!'/>
+            </div>
         </div>
     )
 }
