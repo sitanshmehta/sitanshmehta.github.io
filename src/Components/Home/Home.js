8ebec532ddf3336926c7c0b2f3b178fef61e0f65
sitanshmehta <s65mehta@uwaterloo.ca>
2023-11-10 18:48:48 -0500

    Version 1 of Homepage with intro

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,10 +7,21 @@ const Home = () => {
     return(
         <div className='home-page'>
             <div className='intro'>
-                <span> Hey I'm <h1 className='name'> Sitansh Mehta</h1> </span>
+                <span className='intro-name'> Hey I'm <h1 className='name'> Sitansh Mehta</h1> </span>
+                <div className='intro-content'>
+                    <span className='intro-content-para'>
+                        I'm a Mechatronics Engineering undergraduate at the University of Waterloo
+                        passionate about Software, Firmware, and Robotics. 
+                    </span>
+                </div>
+                <div className='intro-note'>
+                    <spa>
+                        I'm currently restructuring the site 🧹🛠👷🏽‍♂️-- bare with me!
+                    </spa>
+                </div>
             </div>
-            <div>
-                <img src={profile} alt='Whoops!'/>
+            <div className='profile-image-container'>
+                <img src={profile} alt='Whoops!' className='profile-image'/>
             </div>
         </div>
     )
