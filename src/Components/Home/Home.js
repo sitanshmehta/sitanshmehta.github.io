b4736b7b154c016dde7761cb649791f760ca1eb1
sitanshmehta <s65mehta@uwaterloo.ca>
2024-09-22 22:18:41 -0400

    More home content and fixing styling on phone

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,19 +11,23 @@ const Home = () => {
             <div className='intro'>
                 <h1 className='intro-greeting'>Hey, I'm <span className='name'>Sitansh Mehta</span></h1>
                 <p className='intro-content'>
-                    I'm a Mechatronics Engineering undergraduate at the University of Waterloo and I'm
-                    interested in:
+                    Welcome to my website where I hope to show you more my projects, experiences, and ideas. Communicating complex engineering concepts is something I’m actively working on and this website this a part of that journey.
+                </p>
+                <p className='intro-content'>
+                    I'm a Mechatronics Engineering undergraduate at the University of Waterloo. Most of my experiences are in:
                 </p>
                 <ul className='interest-list'>
                     <li>Web and Desktop Development</li>
                     <li>Machine Learning</li>
                     <li>Computer Vision</li>
                     <li>System Programming</li>
-                    <li>Low Level Hardware Programming</li>
                     <li>Control Systems</li>
                 </ul>
                 <p className='intro-content'>
-                    I’m excited to be returning to Skygauge Robotics as a Software Engineer Co-op for Fall 2024, and I’m actively seeking my next opportunity for Summer 2025 (April - August). Check out my experiences!
+                    I’m returning to Skygauge Robotics as a Software Engineer Co-op this Fall, where I'm working on an Extended Kalman Filter (EKF), a Battery Management System, and various side projects.
+                </p>
+                <p className='intro-content'>
+                    I’m also actively seeking opportunities for <strong>Summer 2025 (April - August)</strong>. I'm extremely interested in Robotics (especially in Brain-Computer Interfaces), Software, and Machine Learning. This term at Skygauge is super exciting because I'm working on all my core interests. Check out my experiences!
                 </p>
                 <p className='intro-note'>
                     <div className='intro-item'>
